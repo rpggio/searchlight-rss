228831482f7e6f7eb70f37b12c5e8b21444535f1
Ryan Smith <rys@pobox.com>
2023-12-14 18:05:34 -0800

    Trying RSS change

--- scripts/create-rss.ts
+++ scripts/create-rss.ts
@@ -38,6 +38,35 @@ function generatePodcastRSS(teachings: Teaching[]): string {
    return rssStart + rssItems + rssEnd;
 }
 
+function generatePodcastRSS2(teachings: Teaching[]): string {
+   let itemsXML = teachings.map(teaching => `
+     <item>
+       <title>${teaching.title}</title>
+       <link>${teaching.mediaURL}</link>
+       <pubDate>${new Date(teaching.date).toUTCString()}</pubDate>
+       <enclosure url="${teaching.mediaURL}" type="${teaching.mediaURL}"/>
+       <guid isPermaLink="false">searchlight-${teaching.teachingNumber}</guid>
+       <itunes:author>Jon Courson</itunes:author>
+       <itunes:explicit>no</itunes:explicit>
+     </item>`).join('');
+     //<description>${teaching.title}</description> 
+ //<itunes:duration>${teaching.itunesDuration}</itunes:duration>
+
+   const rssXML = `
+ <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+ <channel>
+   <title>SearchLight with Jon Courson</title>
+   <link>https://www.joncourson.com</link>
+   <description>Thru-The-Bible teachings by Jon Courson</description>
+   <language>en-us</language>
+${itemsXML}
+ </channel>
+ </rss>`;
+ 
+   return rssXML;
+ }
+
+
 function loadTeachingsFromCSV(csvPath: string): Teaching[] {
    const csv = fs.readFileSync(csvPath, 'utf8')
    const parsed = Papa.parse(csv, { header: true }).data
@@ -46,7 +75,7 @@ function loadTeachingsFromCSV(csvPath: string): Teaching[] {
 
 function main() {
    const teachings = loadTeachingsFromCSV('data/genesis.csv');
-   const rss = generatePodcastRSS(teachings);
+   const rss = generatePodcastRSS2(teachings);
    fs.writeFileSync('data/genesis.xml', rss);
 }
 
