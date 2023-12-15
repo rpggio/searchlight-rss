d9206c3323b313054389358d08b860d681e4be9b
Ryan Smith <rys@pobox.com>
2023-12-15 10:04:42 -0800

    RSS format

--- src/scripts/gen-rss.ts
+++ src/scripts/gen-rss.ts
@@ -1,11 +1,11 @@
 
-import * as fs from 'fs'
-import Papa from 'papaparse'
-import { Teaching, TeachingFeed } from '../types'
+import * as fs from 'fs';
+import { TeachingFeed } from '../types';
 
-function generateRss(teachings: Teaching[]): string {
+function generateRss(feed: TeachingFeed): string {
   let rssItems = '';
-  teachings.forEach(teaching => {
+
+  feed.teachings.forEach(teaching => {
 
     if (!teaching.media?.source) {
       return
@@ -17,30 +17,54 @@ function generateRss(teachings: Teaching[]): string {
     }
 
     rssItems += `
-    <item>
-      <title>${teaching.title || 'Untitled Teaching'}</title>
-      <link>${teaching.media.link}</link>
-      <description>${teaching.title}</description> 
-      <guid isPermaLink="false">${teaching.media.source.file}</guid>
-      <pubDate>${new Date(teaching.date).toUTCString()}</pubDate>
-      <description>${teaching.passage || 'Bible Passage'} - ${teaching.event || 'Event'}</description>
-      <enclosure url="${teaching.media.source.file}" type="${teaching.media.source.type}"/>
-      <itunes:author>Jon Courson</itunes:author>
-      <itunes:explicit>no</itunes:explicit>
-      <itunes:duration>${teaching.media.duration}</itunes:duration>
-    </item>`;
+      <item>
+        <title>${teaching.title || 'Untitled Teaching'}</title>
+        <link>${teaching.media.link}</link>
+        <description>${teaching.title}</description> 
+        <guid isPermaLink="false">${teaching.media.source.file}</guid>
+        <pubDate>${new Date(teaching.date).toUTCString()}</pubDate>
+        <description>${teaching.passage || 'Bible Passage'} - ${teaching.event || 'Event'}</description>
+        <enclosure url="${teaching.media.source.file}" length="${teaching.media.source.filesize}" type="${teaching.media.source.type}"/>
+        <itunes:author>Jon Courson</itunes:author>
+        <itunes:explicit>false</itunes:explicit>
+        <itunes:duration>${teaching.media.duration}</itunes:duration>
+      </item>`;
   });
 
+  const lastTeaching = feed.teachings[feed.teachings.length - 1]
+  const pubDate = lastTeaching ? new Date(lastTeaching.date).toUTCString() : null
+
   return `<?xml version="1.0" encoding="UTF-8"?>
-   <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+   <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
    <channel>
-     <title>SearchLight with Jon Courson</title>
-     <link>https://www.joncourson.com</link>
-     <description>Thru-The-Bible teachings by Jon Courson</description>
-     <language>en-us</language>
-  ${rssItems}
-   </channel>
-   </rss>`;
+    <title>${feed.title}</title>
+    <link>https://www.joncourson.com</link>
+    <description>Thru-The-Bible teachings by Jon Courson</description>
+    <language>en-us</language>
+    <copyright>Copyright 2023 Searchlight</copyright>
+    <pubDate>${pubDate}</pubDate>
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+    <image>
+      <title>SearchLight with Jon Courson</title>
+      <url>https://slpodcast.blob.core.windows.net/podcast/podcastimage144.jpg</url>
+      <link>http://www.joncourson.com</link>
+    </image>
+    <itunes:author>Jon Courson</itunes:author>
+    <itunes:type>serial</itunes:type>
+    <itunes:image href="https://slpodcast.blob.core.windows.net/podcast/podcastimage.jpg"/>
+    <itunes:category text="Religion & Spirituality">
+      <itunes:category text="Christianity"/>
+    </itunes:category>
+    <itunes:owner>
+      <itunes:name>Searchlight</itunes:name>
+      <itunes:email>podcast@joncourson.com</itunes:email>
+    </itunes:owner>
+    <itunes:explicit>false</itunes:explicit>
+    <itunes:keywords>courson, jon, jon courson, searchlight,jesus, bible, christian, christianity</itunes:keywords>
+    <atom:link href="" rel="self" type="application/rss+xml"/>
+${rssItems}
+  </channel>
+  </rss>`;
 }
 
 // function loadTeachingsFromCSV(csvPath: string): Teaching[] {
@@ -52,7 +76,7 @@ function generateRss(teachings: Teaching[]): string {
 function main() {
   const book = 'genesis'
   const feed = JSON.parse(fs.readFileSync(`data/${book}.json`, 'utf8')) as TeachingFeed;
-  const rss = generateRss(feed.teachings);
+  const rss = generateRss(feed);
   fs.writeFileSync(`data/${book}.xml`, rss);
 }
 
