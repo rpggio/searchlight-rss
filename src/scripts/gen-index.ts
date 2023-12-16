65183b15382b18617ed93c7625c5e421767f5db4
Ryan Smith <rys@pobox.com>
2023-12-15 21:26:37 -0800

    podcast URL

--- src/scripts/gen-index.ts
+++ src/scripts/gen-index.ts
@@ -6,7 +6,7 @@ function main() {
    const bookLinks = bibleBooks.map(book => {
       const slug = fileNameSlug(book)
       const feedFile = `${slug}.xml`
-      const feedUrl = `feed://rpggio.github.io/searchlight-rss/${feedFile}`
+      const feedUrl = `podcast://rpggio.github.io/searchlight-rss/${feedFile}`
       return `       <a href="${feedUrl}"><div class="book"><span>${book}</span></div></a>`
    })
 
